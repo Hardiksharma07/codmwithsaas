60ece84e463921d2191ffc4d5acce58b21c48ef7
Hardiksharma07 <156889739+Hardiksharma07@users.noreply.github.com>
2026-05-14 13:14:52 +0100

    Integrate SEO component in About page
    Added SEO component with metadata for the About page.

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -7,21 +7,29 @@ import SecondSection from "../../AboutComponents/SecondSection/SecondSection";
 import ThirdSection from "../../AboutComponents/ThirdSection/ThirdSection";
 import WhatWeDo from "../../AboutComponents/WhatWeDo/WhatWeDo";
 import BlogSection from "../../components/BlogSection/BlogSection";
+import SEO from "../../SeoData/SEO";
 
 
 function About() {
   return (
     <>
-  <FirstSection/>
-  <SecondSection/>
-  <WhatWeDo/>
-  {/* <ThirdSection/> */}
-  <FourthSection/>
-   <FounderSection/>
-  {/* <Insurance/> */}
-  <BlogSection/>
-  {/* <FifthSection/> */}
-  </>
+      <SEO
+        title="About CODM Software | Expert Salesforce & AI Solutions Partner"
+        description="Learn about CODM Software - a trusted Salesforce partner specializing in CRM development, AI solutions, and enterprise software innovation since 2015."
+        url="https://codmsoftware.co.uk/about"
+        keywords="About CODM Software, Salesforce partner, CRM experts, software development company, AI solutions provider, enterprise software"
+        image="https://codmsoftware.co.uk/images/about-hero.jpg"
+      />
+      <FirstSection/>
+      <SecondSection/>
+      <WhatWeDo/>
+      {/* <ThirdSection/> */}
+      <FourthSection/>
+      <FounderSection/>
+      {/* <Insurance/> */}
+      <BlogSection/>
+      {/* <FifthSection/> */}
+    </>
   )
 }
 
